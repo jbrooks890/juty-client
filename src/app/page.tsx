4d1e7bca0be594ed1c563f576a9f58a26c46a49c
Julian Brooks <jbrooks890@gmail.com>
2025-04-07 02:08:10 -0400

    wip: brainstorming homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,13 +37,22 @@ export default function Home() {
         <Section title="Landing" className="bg-gray-100 pt-16">
           JuTy
         </Section>
-        <Section title="Call to Action" className="bg-gray-200">
+        <Section title="For Sale" className="bg-gray-200">
+          <h2>2056 Gober Ave</h2>
+          <List
+            bullet="–"
+            content={"Move-In Ready/1200 sqft./3 bed, 2.5 bath/2 Car Garage".split(
+              "/"
+            )}
+          />
+        </Section>
+        <Section title="Transformation" titled className="bg-gray-300">
           Get A Quote
         </Section>
-        <Section title="Gallery" className="bg-gray-300">
+        <Section title="Gallery" className="bg-gray-400">
           Gallery
         </Section>
-        <Section title="Testimonials" className="bg-gray-400">
+        <Section title="Testimonials" className="bg-gray-500">
           Testimonials
         </Section>
       </Page>
